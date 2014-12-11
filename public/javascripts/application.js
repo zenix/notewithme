207a09e489b9d6777f0275806a9e655c416b89a7
Jari Timonen <jari.timonen@zeniitti.net>
2014-12-11 12:33:46 +0200

    remove some functions

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -132,34 +132,4 @@ application.controller('MainCtrl', ['$scope', '$window', 'SocketIoService',
                 canvas.renderAll();
             });
         });
-
-
-        $scope.keypressedBackSpace = function (keycode) {
-            if (keycode == 8) {
-                msg.text = '';
-                msg.cursorposition = $scope.textCursorPosition.get;
-                socket.emit('removechar', msg);
-            }
-        };
-
-        $scope.keypressed = function (keyCode) {
-            msg.text = String.fromCharCode(keyCode);
-            msg.cursorposition = $scope.textCursorPosition.get;
-            socket.emit('addchar', msg);
-        }
-
-        socket.on('addchar', function (message) {
-            $scope.message = message;
-            var text = $scope.text;
-            var cursorposition = message.cursorposition;
-            $scope.text = text.substr(0, cursorposition) + message.text + text.substr(cursorposition, text.length);
-            $scope.$apply();
-        });
-
-        socket.on('removechar', function (message) {
-            var text = $scope.text;
-            var cursorposition = message.cursorposition;
-            $scope.text = text.substring(0, cursorposition - 1) + text.substr(cursorposition, text.length);
-            $scope.$apply();
-        });
     }]);
