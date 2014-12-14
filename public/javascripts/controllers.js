e0893733738eba921fe4179d14ae1921233c5017
Jari Timonen <jari.timonen@zeniitti.net>
2014-12-14 19:54:59 +0200

    use compile

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -10,18 +10,24 @@ noteWithMeControllers.controller('mainController', ['$scope','$location','UserSe
     }
 }]);
 
-noteWithMeControllers.controller('roomController', ['$scope', '$routeParams', '$window', 'SocketIoService','UserService','FabricService', function($scope, $routeParams, $window, SocketIoService, UserService,FabricService){
+noteWithMeControllers.controller('roomController', ['$scope', '$location', '$routeParams', '$compile', '$window', 'SocketIoService','UserService','FabricService', function($scope, $location, $routeParams, $compile, $window, SocketIoService, UserService,FabricService){
 
     $scope.start = function(user){
         UserService.user().name = user.name;
         UserService.user().room = $routeParams.roomName;
-        angular.element('#startModal').clone().modal('close');
+        modalWith('hide');
         FabricService.start($scope);
     }
 
     if(UserService.isEmpty()){
-        angular.element('#startModal').clone().modal('show');
+        modalWith('show');
     }else{
         FabricService.start($scope);
+    }
+
+    function modalWith(action) {
+        var element = angular.element('#startModal').clone();
+        $compile(element)($scope);
+        element.modal(action);
     }
 }]);
