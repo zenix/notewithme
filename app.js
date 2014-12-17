1d109646f70ce43a6b6f8e78e6d14cce3df5d9e4
Jari Timonen <jari.timonen@elisa.fi>
2014-12-17 13:04:31 +0200

    moved stuff and removed ejs

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 var http = require('http').Server(app);
@@ -57,20 +54,15 @@ io.on('connection', function(socket){
 
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(express.static(__dirname + '/public_html'));
+
+app.use("*", function (req, res) {
+    res.sendFile(__dirname + '/public_html/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -79,29 +71,7 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// TODO: error handlers
 
 var debug = require('debug')('notewithme');
 //var app = require('../app');
