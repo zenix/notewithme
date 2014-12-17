3e4a69f5233894e6ade3630f192eeef505f7b466
Jari Timonen <jari.timonen@elisa.fi>
2014-12-17 13:47:21 +0200

    added tutorial url

--- tests/spec.js
+++ tests/spec.js
@@ -1,4 +1,5 @@
 // spec.js
+//https://github.com/angular/protractor/blob/master/docs/tutorial.md
 describe('angularjs homepage', function() {
   it('should have a title', function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
