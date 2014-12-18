21cd82e6602e7b044b191a6ffc0233290248b76c
Jari Timonen <jari.timonen@zeniitti.net>
2014-12-18 21:33:01 +0200

    fixed paths. added one test

--- tests/spec.js
+++ tests/spec.js
@@ -1,9 +1,8 @@
 // spec.js
 //https://github.com/angular/protractor/blob/master/docs/tutorial.md
 describe('angularjs homepage', function() {
-  it('should have a title', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
-
-    expect(browser.getTitle()).toEqual('Super Calculator');
+  it('front page loads', function() {
+    browser.get('http://localhost:3000/#/');
+    expect(browser.getTitle()).toEqual('notewithme');
   });
 });
