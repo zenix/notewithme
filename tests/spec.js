0a3b833e390d49ce418478f1aece951bfec9326b
Jari Timonen <jari.timonen@zeniitti.net>
2014-12-18 21:42:54 +0200

    fixes

--- tests/spec.js
+++ tests/spec.js
@@ -1,6 +1,6 @@
 // spec.js
 //https://github.com/angular/protractor/blob/master/docs/tutorial.md
-describe('angularjs homepage', function() {
+describe('in main page', function() {
   it('front page loads', function() {
     browser.get('http://localhost:3000/#/');
     expect(browser.getTitle()).toEqual('notewithme');
