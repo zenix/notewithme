32df8875da8c7e694a262cc034dd77f02b999137
Jari Timonen <jari.timonen@elisa.fi>
2014-12-19 13:08:41 +0200

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function (grunt) {
+ 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+ 
+        clean: ["dist", '.tmp'],
+ 
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'public_html/',
+                src: ['**', '!js/**', '!lib/**', '!**/*.css'],
+                dest: 'dist/'
+            }//,
+            //shims: {
+              //  expand: true,
+           //     cwd: 'app/lib/webshim/shims',
+            //    src: ['**'],
+            //    dest: 'dist/js/shims'
+           // }
+        },
+ 
+        rev: {
+            files: {
+                src: ['dist/**/*.{js,css}', '!dist/js/shims/**']
+            }
+        },
+ 
+        useminPrepare: {
+            html: 'public_html/index.html'
+        },
+ 
+        usemin: {
+            html: ['dist/index.html']
+        },
+ 
+        uglify: {
+            options: {
+                report: 'min',
+                mangle: false
+            }
+        }
+    });
+ 
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-rev');
+    grunt.loadNpmTasks('grunt-usemin');
+ 
+    // Tell Grunt what to do when we type "grunt" into the terminal
+    grunt.registerTask('default', [
+        'copy', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'rev', 'usemin'
+    ]);
+};
