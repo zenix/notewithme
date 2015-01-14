39af1fed8954e075d34840749efb472cd30f2394
Jari Timonen <jari.timonen@elisa.fi>
2015-01-14 07:23:41 +0200

    Added some texts

--- public_html/javascripts/maincontroller.js
+++ public_html/javascripts/maincontroller.js
@@ -6,4 +6,13 @@ nwmApplication.controller('mainController', ['$scope','$location','UserService',
         UserService.user().room = user.room;
         $location.path('/room/'+user.room);
     }
+
+    angular.element('.mainContent').html('This is an early alpha version of notewithme.com. This service is meant for collaboration in meetings/meet-ups. Try it out!');
+    $scope.shareClick = function(){
+        angular.element('.mainContent').html('Copy paste link from address bar and send it to co-workers');
+    }
+
+    $scope.collaborateClick = function(){
+           angular.element('.mainContent').html('Start making notes and draw. Your fellow collaborators see results instantly.');
+       }
 }]);
