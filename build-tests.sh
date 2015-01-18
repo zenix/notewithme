b429b58a58edd96abe33ff8766c190bc20352772
Jari Timonen <jari.timonen@zeniitti.net>
2015-01-18 19:27:26 +0200

    tests

--- build-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-npm install
-node_modules/.bin/webdriver-manager update
-node_modules/.bin/webdriver-manager start
-./node_modules/protractor/bin/protractor protractor-conf.js
---
+++ ci-build-tests.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+npm install
+bower instal
+node_modules/protractor/bin/protractor protractor-conf.js
