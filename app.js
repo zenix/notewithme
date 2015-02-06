547e86286ed04d9bda1e75df19db17b4f9239879
Jari Timonen <jari.timonen@elisa.fi>
2015-02-06 11:01:27 +0200

    format

--- app.js
+++ app.js
@@ -5,97 +5,76 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var redis = require("redis");
-
 var app = express();
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect', function(msg){
+    socket.on('disconnect', function (msg) {
         console.log('user disconnected');
     });
-
-    socket.on('joinRoom', function(msg){
+    socket.on('joinRoom', function (msg) {
         console.log(msg.name + ' joined ' + getRoom(msg));
         socket.user = msg;
         socket.join(getRoom(msg));
         var clientsConnected = Object.keys(io.sockets.connected).length;
-        if(clientsConnected > 1) {
+        if (clientsConnected > 1) {
             var clients_in_the_room = io.sockets.adapter.rooms[getRoom(msg)];
             for (var clientId in clients_in_the_room) {
-                if(clientId != socket.id){
+                if (clientId != socket.id) {
                     var client_socket = io.sockets.connected[clientId];
-                    client_socket.emit('syncClient', {clientId:socket.id})
+                    client_socket.emit('syncClient', {clientId: socket.id})
                     break;
                 }
             }
-        }else if(clientsConnected == 1){
+        } else if (clientsConnected == 1) {
             var redisClient = redis.createClient();
             var room = getRoom(msg);
-            redisClient.get(room, function(err, canvas){
+            redisClient.get(room, function (err, canvas) {
                 console.log("Getting saved canvas:  " + room);
-                io.sockets.in(room).emit('updateCanvas', {canvas:canvas});
+                io.sockets.in(room).emit('updateCanvas', {canvas: canvas});
             })
         }
     });
-
-    socket.on('syncClient', function(msg){
-        io.sockets.connected[msg.clientId].emit('updateCanvas', {canvas:msg.canvas});
+    socket.on('syncClient', function (msg) {
+        io.sockets.connected[msg.clientId].emit('updateCanvas', {canvas: msg.canvas});
     })
-
-    socket.on('saveCanvas', function(msg){
+    socket.on('saveCanvas', function (msg) {
         var redisClient = redis.createClient();
         var room = getRoom(msg);
         redisClient.set(room, msg.canvas, redis.print);
-        console.log("saving.. " + room);
+        console.log("saving: " + room);
     })
-
     addListener('addObject');
     addListener('removeObject');
     addListener('writing');
     addListener('scaling');
     addListener('rotating');
     addListener('moving');
-
-    function addListener(name){
-        socket.on(name, function(msg){
+    function addListener(name) {
+        socket.on(name, function (msg) {
             var user = socket.user;
-            if(user) {
+            if (user) {
                 socket.broadcast.to(getRoom(user)).emit(name, msg);
             }
         });
     };
-
-    function getRoom(user){
+    function getRoom(user) {
         return user.randomString + "/" + user.title;
     }
 });
-
-
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-
 app.use(express.static(__dirname + '/public_html'));
-
 app.use("*", function (req, res) {
     res.sendFile(__dirname + '/public_html/index.html');
 });
-
-
 // TODO: error handlers
-
 var debug = require('debug')('notewithme');
 //var app = require('../app');
-
 app.set('port', process.env.PORT || 3000);
-
-var server = http.listen(app.get('port'), function() {
+var server = http.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
-
-
 module.exports = app;
