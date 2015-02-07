a2600e277f604564230a766bc444a74f07bc964f
Jari Timonen <jari.timonen@zeniitti.net>
2015-02-07 11:50:17 +0200

    format fix test. add starting of webdriver

---
+++ ci-build-start-webdriver.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+npm install
+node_modules/protractor/bin/webdriver-manager update
+node_modules/protractor/bin/webdriver-manager start
--- tests/spec.js
+++ tests/spec.js
@@ -1,18 +1,19 @@
 // spec.js
 //https://github.com/angular/protractor/blob/master/docs/tutorial.md
-describe('in main page', function() {
-  beforeEach(function(){
-    browser.get('http://localhost:3000/#/');
-  })
+describe('in main page', function () {
+    beforeEach(function () {
+        browser.get('http://localhost:3000/#/');
+    })
 
-  it('title is correct', function() {
-    expect(browser.getTitle()).toEqual('notewithme.com - start, share and collaborate');
-  });
-
-  it('clicking start button modal opens', function(){
-    element(by.css('.startModal')).click().then(function(){
-      browser.sleep(300)
-      expect(element(by.css('.modal-title')).getText()).toEqual('Just enter your name and room - you\'re free to start collaborating.');
+    it('title is correct', function () {
+        expect(browser.getTitle()).toEqual('notewithme.com - start, share and collaborate');
     });
-  })
+
+    it('clicking start button modal opens', function () {
+        element(by.css('.startModal')).click().then(function () {
+            browser.sleep(300);
+            expect(element(by.css('.modal-title')).getText()).toEqual('Just enter your name and room and you\'re free to start collaborating.');
+
+        });
+    })
 });
