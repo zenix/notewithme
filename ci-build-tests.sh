28b3810181d220be8ef81b00e0b9d3061293e199
Jari Timonen <jari.timonen@zeniitti.net>
2015-02-08 17:14:24 +0200

    comment out

--- ci-build-tests.sh
+++ ci-build-tests.sh
@@ -4,7 +4,7 @@ npm install gulp
 bower install
 node_modules/gulp/bin/gulp.js
 node dist/app &
-node_modules/protractor/bin/webdriver-manager update
-node_modules/protractor/bin/webdriver-manager start &
-sleep 3
+#node_modules/protractor/bin/webdriver-manager update
+#node_modules/protractor/bin/webdriver-manager start &
+#sleep 3
 node_modules/protractor/bin/protractor protractor-conf.js
