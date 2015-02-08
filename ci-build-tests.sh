9a452106662a5bd08f58afa81a8481b96c27079c
Jari Timonen <jari.timonen@zeniitti.net>
2015-02-08 14:36:01 +0200

    ups

--- ci-build-tests.sh
+++ ci-build-tests.sh
@@ -6,5 +6,5 @@ node_modules/gulp/bin/gulp.js
 node dist/app &
 node_modules/protractor/bin/webdriver-manager update
 node_modules/protractor/bin/webdriver-manager start &
-sleep 3000
+sleep 3
 node_modules/protractor/bin/protractor protractor-conf.js
