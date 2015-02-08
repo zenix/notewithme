7d861952723de65bfc11caa50d50af70be1f8dc8
Jari Timonen <jari.timonen@zeniitti.net>
2015-02-08 13:19:23 +0200

    added defaults

--- protractor-conf.js
+++ protractor-conf.js
@@ -1,5 +1,9 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['tests/*.js'],
-    framework: 'jasmine'
+    framework: 'jasmine',
+    capabilities: {
+        browserName: 'chrome',
+        platform: 'LINUX'
+    }
 }
