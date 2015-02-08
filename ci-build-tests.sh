fda407d564e7c34977627513421f8f914ea1fd1b
Jari Timonen <jari.timonen@zeniitti.net>
2015-02-08 14:28:06 +0200

    use protractor selenium

--- ci-build-tests.sh
+++ ci-build-tests.sh
@@ -4,4 +4,6 @@ npm install gulp
 bower install
 node_modules/gulp/bin/gulp.js
 node dist/app &
+node_modules/protractor/bin/webdriver-manager update
+node_modules/protractor/bin/webdriver-manager start &
 node_modules/protractor/bin/protractor protractor-conf.js
