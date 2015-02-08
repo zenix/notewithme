482626ba7389d2dccaf181070238556a00249172
Jari Timonen <jari.timonen@zeniitti.net>
2015-02-08 14:34:52 +0200

    sleep for a while

--- ci-build-tests.sh
+++ ci-build-tests.sh
@@ -6,4 +6,5 @@ node_modules/gulp/bin/gulp.js
 node dist/app &
 node_modules/protractor/bin/webdriver-manager update
 node_modules/protractor/bin/webdriver-manager start &
+sleep 3000
 node_modules/protractor/bin/protractor protractor-conf.js
