6c10c93a69c2957056efeb27742818938c2c2e97
Jari Timonen <jari.timonen@zeniitti.net>
2015-02-23 21:04:28 +0200

    add todos

--- public_html/javascripts/listenerservice.js
+++ public_html/javascripts/listenerservice.js
@@ -73,6 +73,8 @@ nwmApplication.service('ListenerService', ['$window', 'FabricService', 'SocketIo
                 });
             });
         }
+        //TODO: refactor
+        //TODO: When group is selected, and someone is moving one of those objects -> needs to deactivate group
         function movingMessage(event) {
             if(fabricObjectToAttach.type === 'group'){
                 _.forEach(fabricObjectToAttach._objects, function(object){
