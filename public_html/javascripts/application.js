5bb0f23b57830c67cfe01f3c3afb1980333ee3d2
Jari Timonen <jari.timonen@zeniitti.net>
2015-02-26 08:14:29 +0200

    added ng-contentful config

--- public_html/javascripts/application.js
+++ public_html/javascripts/application.js
@@ -1,9 +1,12 @@
 'use strict';
-var nwmApplication = angular.module('notewithme', ['ngRoute','djds4rce.angular-socialshare']);
+var nwmApplication = angular.module('notewithme', ['ngRoute','djds4rce.angular-socialshare','ng-contentful']);
 nwmApplication.run(['$FB', function($FB){
     $FB.init('1777278692496422');
 }]);
-nwmApplication.config(['$routeProvider', '$compileProvider', function ($routeProvider, $compileProvider) {
+nwmApplication.config(['$routeProvider', '$compileProvider','contentfulClientProvider', function ($routeProvider, $compileProvider,contentfulClientProvider) {
+    contentfulClientProvider.setSpaceId('52e2zlhwamdy');
+    contentfulClientProvider.setAccessToken('a54892f18ee79d9de92bd2d4aaf1a690ebedf5de3a585ef21194ae4e565d8403');
+
     $compileProvider.aHrefSanitizationWhitelist(/^data:image\//);
     $routeProvider.
         when('/r/:randomString/:room', {
