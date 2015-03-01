4b2cc3dc61e654d3623bec358df32cfa6210a29e
Jari Timonen <jari.timonen@zeniitti.net>
2015-03-01 09:45:34 +0200

    add hashbang

--- public_html/javascripts/application.js
+++ public_html/javascripts/application.js
@@ -1,11 +1,13 @@
 'use strict';
 var nwmApplication = angular.module('notewithme', ['ngRoute','ng-contentful', 'hc.marked']);
 
-nwmApplication.config(['$routeProvider', '$compileProvider','contentfulClientProvider','markedProvider', function ($routeProvider, $compileProvider,contentfulClientProvider, markedProvider) {
+nwmApplication.config(['$routeProvider', '$compileProvider','contentfulClientProvider','markedProvider','$locationProvider', function ($routeProvider, $compileProvider,contentfulClientProvider, markedProvider, $locationProvider) {
     markedProvider.setOptions({gfm: true})
 
     contentfulClientProvider.setSpaceId('52e2zlhwamdy');
     contentfulClientProvider.setAccessToken('a54892f18ee79d9de92bd2d4aaf1a690ebedf5de3a585ef21194ae4e565d8403');
+
+    $locationProvider.hashPrefix('!');
 
     $compileProvider.aHrefSanitizationWhitelist(/^data:image\//);
     $routeProvider.
