950e6af03b0f51240cb04a199f8a8b04a6f95578
Jari Timonen <jari.timonen@elisa.fi>
2015-03-03 08:57:24 +0200

    moved controller logic to directive for give it a try

--- public_html/javascripts/giveitatry.js
+++ public_html/javascripts/giveitatry.js
@@ -1,11 +1,28 @@
-nwmApplication.directive('giveitatry', [function () {
+nwmApplication.directive('giveitatry', ['UserService','Utils','$location', function (UserService,Utils,$location) {
     return {
         restrict: 'E',
-        scope: {
-        },
-        link:function($scope){
+        scope: {},
+        link: function ($scope) {
         },
         controller: function ($scope) {
+            $scope.start = function (user) {
+                UserService.user().name = user.name;
+                UserService.user().randomString = Utils.randomString() + Utils.randomString();
+                UserService.user().room = user.room;
+                trackStart();
+                $location.path('/r/' + UserService.user().randomString + '/' + user.room);
+            }
+            function trackStart() {
+                if (window.useAnalytics) {
+                    ga('send', 'event', 'User Path', 'Modal', 'Start');
+                }
+            }
+
+            $scope.trackModal = function () {
+                if (window.useAnalytics) {
+                    ga('send', 'event', 'User Path', 'Main Page', 'Open Modal');
+                }
+            }
         },
         templateUrl: 'directives/giveitatry.html'
     }
--- public_html/javascripts/maincontroller.js
+++ public_html/javascripts/maincontroller.js
@@ -3,22 +3,4 @@ nwmApplication.controller('mainController', ['$scope', '$location', 'UserService
     if(window.useAnalytics) {
         ga('send', 'event', 'User Path', 'Main Page', 'Load');
     }
-    $scope.start = function (user) {
-        UserService.user().name = user.name;
-        UserService.user().randomString = Utils.randomString() + Utils.randomString();
-        UserService.user().room = user.room;
-        trackStart();
-        $location.path('/r/' + UserService.user().randomString + '/' + user.room);
-    }
-    function trackStart() {
-        if(window.useAnalytics) {
-            ga('send', 'event', 'User Path', 'Modal', 'Start');
-        }
-    }
-
-    $scope.trackModal = function () {
-        if(window.useAnalytics) {
-            ga('send', 'event', 'User Path', 'Main Page', 'Open Modal');
-        }
-    }
 }]);
