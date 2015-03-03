80486c0170311ac13e5b336def0d473b2eef5468
Jari Timonen <jari.timonen@elisa.fi>
2015-03-03 07:47:51 +0200

    added row hr directive

---
+++ public_html/javascripts/rowhr.js
@@ -0,0 +1,13 @@
+nwmApplication.directive('rowhr', [function () {
+    return {
+        restrict: 'E',
+        scope: {
+            limit: '='
+        },
+        link:function($scope){
+        },
+        controller: function ($scope) {
+        },
+        templateUrl: 'directives/rowhr.html'
+    }
+}]);
